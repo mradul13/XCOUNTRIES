85a571d58e0ee9268d6b290584fda560fd79dd95
mradul13 <mradulchourasiya@gmail.com>
2024-06-09 01:30:40 +0530

    bug fixed

--- xcountries/src/api/api.js
+++ xcountries/src/api/api.js
@@ -4,7 +4,7 @@ const PROXY_API_ENDPOINT = "/.netlify/functions/fetch-api";
 
 export const fetchAllCountries = async ()=>{
     try {
-        const response = await axios.get(`${API_ENDPOINT}`);
+        const response = await axios.get(`${PROXY_API_ENDPOINT}`);
         return response.data
     }
     catch(error){
